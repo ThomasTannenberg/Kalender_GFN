07abdc25a105d83a42324fad5e1150cb24ad1cba
Thomas Tannenberg <Thomas@Tannenberg.online>
2024-03-17 19:40:51 +0100

    Projektdaten added

---
+++ create_user.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+$username = $_POST['username'];
+
+$passwordHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+try {
+
+$stmt = $dbh->prepare("INSERT INTO benutzer (username, passwort, email) VALUES (:username, :passwort, :email)");
+$stmt->bindParam(':username', $username);
+$stmt->bindParam(':passwort', $passwordHash);
+$stmt->bindParam(':email', $_POST['email']);
+$stmt->execute();
+
+// Weiterleiten
+echo "Benutzerkonto erfolgreich erstellt.";
+} catch (PDOException $e) {
+echo "Fehler bei der Kontoerstellung: " . $e->getMessage();
+}
+}
